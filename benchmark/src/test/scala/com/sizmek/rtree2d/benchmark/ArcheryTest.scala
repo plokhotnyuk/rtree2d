5a6d9646f73ca0b7e797bf3981c2c58d9a5de20c
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2018-06-14 09:39:20 +0200

    Split parameters for overlap and rectangle size

--- benchmark/src/test/scala/com/sizmek/rtree2d/benchmark/ArcheryTest.scala
+++ benchmark/src/test/scala/com/sizmek/rtree2d/benchmark/ArcheryTest.scala
@@ -13,7 +13,7 @@ class ArcheryTest extends WordSpec with Matchers {
       benchmark.insert.entries.toSeq should contain allElementsOf (benchmark.rtreeEntries ++ benchmark.entriesToAddOrRemove)
       benchmark.remove.entries.toSeq should contain allElementsOf benchmark.rtreeEntries.diff(benchmark.entriesToAddOrRemove)
       (1 to benchmark.size * 2).foreach(_ => benchmark.searchByPoint.size shouldBe 1)
-      (1 to benchmark.size * 2).foreach(_ => benchmark.searchByRectangle.size shouldBe 1)
+      (1 to benchmark.size * 2).foreach(_ => benchmark.searchByRectangle.size should be >= 1)
     }
   }
 }
--- benchmark/src/test/scala/com/sizmek/rtree2d/benchmark/DavidMotenRTreeTest.scala
+++ benchmark/src/test/scala/com/sizmek/rtree2d/benchmark/DavidMotenRTreeTest.scala
@@ -15,7 +15,7 @@ class DavidMotenRTreeTest extends WordSpec with Matchers {
       benchmark.insert.entries().toBlocking.toIterable.asScala.toSeq should contain allElementsOf (benchmark.rtreeEntries ++ benchmark.entriesToAddOrRemove)
       benchmark.remove.entries().toBlocking.toIterable.asScala.toSeq should contain allElementsOf benchmark.rtreeEntries.diff(benchmark.entriesToAddOrRemove)
       (1 to benchmark.size * 2).foreach(_ => benchmark.searchByPoint.size shouldBe 1)
-      (1 to benchmark.size * 2).foreach(_ => benchmark.searchByRectangle.size shouldBe 1)
+      (1 to benchmark.size * 2).foreach(_ => benchmark.searchByRectangle.size should be >= 1)
     }
   }
 }
