74da827d7babf4897c956381adf3b2e306d2e129
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-01-12 12:02:21 +0100

    Clean up of code for `chart` task + update jsoniter-scala to v0.38.0

--- project/Bencharts.scala
+++ project/Bencharts.scala
@@ -10,16 +10,15 @@ import org.jfree.data.xy.{YIntervalSeries, YIntervalSeriesCollection}
 import sbt._
 import com.github.plokhotnyuk.jsoniter_scala.macros._
 import com.github.plokhotnyuk.jsoniter_scala.core._
+import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker._
 
-import scala.collection.{SortedMap, immutable}
+import scala.collection.SortedMap
 
-case class BenchmarkMetric(score: Double, scoreConfidence: (Double, Double))
-
-case class BenchmarkResult(benchmark: String, params: Map[String, String], primaryMetric: BenchmarkMetric)
-
+/**
+  * Based on a great work of Julien Richard-Foy (@julienrf):
+  * https://github.com/scala/collection-strawman/blame/master/project/Bencharts.scala
+  */
 object Bencharts {
-  implicit val codec: JsonValueCodec[Seq[BenchmarkResult]] = JsonCodecMaker.make[Seq[BenchmarkResult]](CodecMakerConfig())
-
   /**
     * Generate charts from the result of a JMH execution.
     *
@@ -27,48 +26,45 @@ object Bencharts {
     * with one series of several size param values for each value combination of other params.
     *
     * @param jmhReport JMH results report
+    * @param yAxisTitle The title of a Y-axis
     * @param targetDir Directory in which the images will be written
     */
   def apply(jmhReport: File, yAxisTitle: String, targetDir: File): Unit = {
-    val allResults = readFromArray(IO.readBytes(jmhReport))
+    val allResults = readFromArray(IO.readBytes(jmhReport))(make[Seq[BenchmarkResult]](CodecMakerConfig()))
     val constParams = allResults.flatMap(_.params.toSeq).groupBy(_._1)
       .collect { case (k, kvs) if kvs.distinct.size == 1 => kvs.head }.toSeq
     allResults.groupBy(benchmarkName(constParams)).foreach { case (benchmark, results) =>
-      val seriess = SortedMap(results.groupBy(otherParams(constParams)).toSeq:_*)
-        .map { case (params, iterations) =>
-          val ySeries = new YIntervalSeries(params)
-          // each benchmark has been run with several sizes (10, 100, 1000, etc.)
-          // we add a point for each of these iterations
-          iterations.foreach { iteration =>
-            ySeries.add(
-              iteration.params.get("size").fold(0.0)(_.toDouble),
-              Math.max(iteration.primaryMetric.score, 1.0),
-              Math.max(iteration.primaryMetric.scoreConfidence._1, 1.0),
-              Math.max(iteration.primaryMetric.scoreConfidence._2, 1.0)
-            )
-          }
-          ySeries
+      val dataset = new YIntervalSeriesCollection {
+        SortedMap(results.groupBy(otherParams(constParams)).toSeq:_*).foreach { case (params, iterations) =>
+          addSeries(new YIntervalSeries(params) {
+            iterations.foreach { iteration =>
+              val x = iteration.params.get("size").fold(0.0)(_.toDouble)
+              val y = Math.max(iteration.primaryMetric.score, 1.0)
+              val yLow = Math.max(iteration.primaryMetric.scoreConfidence._1, 1.0)
+              val yHigh = Math.max(iteration.primaryMetric.scoreConfidence._2, 1.0)
+              add(x, y, yLow, yHigh)
+            }
+          })
         }
-      val col = new YIntervalSeriesCollection
-      val renderer = new XYErrorRenderer
-      seriess.zipWithIndex.foreach { case (series, i) =>
-        col.addSeries(series)
-        renderer.setSeriesLinesVisible(i, true)
       }
-      val plot = new XYPlot(col, axis("Size"), axis(yAxisTitle), renderer)
-      plot.setDrawingSupplier(new DefaultDrawingSupplier {
-        override def getNextPaint: Paint = super.getNextPaint match {
-          case x: Color if x.getRed > 200 && x.getGreen > 200 =>
-            new Color(x.getRed, (x.getGreen * 0.8).toInt, x.getBlue, x.getAlpha)
-          case x => x
-        }
-      })
+      val renderer = new XYErrorRenderer {
+        (0 to dataset.getSeriesCount).foreach(i => setSeriesLinesVisible(i, true))
+      }
+      val plot = new XYPlot(dataset, axis("Size"), axis(yAxisTitle), renderer) {
+        setDrawingSupplier(new DefaultDrawingSupplier {
+          override def getNextPaint: Paint = super.getNextPaint match {
+            case x: Color if x.getRed > 200 && x.getGreen > 200 =>
+              new Color(x.getRed, (x.getGreen * 0.8).toInt, x.getBlue, x.getAlpha)
+            case x => x
+          }
+        })
+      }
       val chart = new JFreeChart(benchmark, JFreeChart.DEFAULT_TITLE_FONT, plot, true)
       ImageIO.write(chart.createBufferedImage(1200, 900), "png", targetDir / s"$benchmark.png")
     }
   }
 
-  private def axis(title: String) = new LogarithmicAxis(title) {
+  private def axis(title: String): LogarithmicAxis = new LogarithmicAxis(title) {
     setAllowNegativesFlag(true)
     setNumberFormatOverride(NumberFormat.getInstance())
   }
@@ -88,3 +84,7 @@ object Bencharts {
     }.toSeq.sorted.mkString(s"$benchSuitName[", ",", "]")
   }
 }
+
+case class BenchmarkMetric(score: Double, scoreConfidence: (Double, Double))
+
+case class BenchmarkResult(benchmark: String, params: Map[String, String], primaryMetric: BenchmarkMetric)
