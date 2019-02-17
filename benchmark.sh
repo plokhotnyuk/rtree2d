5513f6671756323529368006d30ba0dba654b93f
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-02-17 11:35:23 +0100

    Fix the benchmark script to do not delete charts

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sbt -java-home /usr/lib/jvm/jdk1.8.0 clean 'charts -jvm /usr/lib/jvm/jdk-11/bin/java -p overlap=1 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=16 -p partToUpdate=0.1 -p geometry=plane .*' 2>&1 | tee plane_overlap_1.txt
-sbt -java-home /usr/lib/jvm/jdk1.8.0 clean 'charts -jvm /usr/lib/jvm/jdk-11/bin/java -p overlap=10 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=16 -p partToUpdate=0.1 -p geometry=plane .*' 2>&1 | tee plane_overlap_10.txt
-sbt -java-home /usr/lib/jvm/jdk1.8.0 clean 'charts -jvm /usr/lib/jvm/jdk-11/bin/java -p overlap=1 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=4,8,16 -p partToUpdate=0.1 -p geometry=spherical RTree2D.*' 2>&1 | tee spherical_overlap_1.txt
-sbt -java-home /usr/lib/jvm/jdk1.8.0 clean 'charts -jvm /usr/lib/jvm/jdk-11/bin/java -p overlap=10 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=4,8,16 -p partToUpdate=0.1 -p geometry=spherical RTree2D.*' 2>&1 | tee spherical_overlap_10.txt
+sbt -java-home /usr/lib/jvm/jdk1.8.0 clean 2>&1
+sbt -java-home /usr/lib/jvm/jdk1.8.0 'charts -jvm /usr/lib/jvm/jdk-11/bin/java -p overlap=1 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=16 -p partToUpdate=0.1 -p geometry=plane .*' 2>&1 | tee plane_overlap_1.txt
+sbt -java-home /usr/lib/jvm/jdk1.8.0 'charts -jvm /usr/lib/jvm/jdk-11/bin/java -p overlap=10 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=16 -p partToUpdate=0.1 -p geometry=plane .*' 2>&1 | tee plane_overlap_10.txt
+sbt -java-home /usr/lib/jvm/jdk1.8.0 'charts -jvm /usr/lib/jvm/jdk-11/bin/java -p overlap=1 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=4,8,16 -p partToUpdate=0.1 -p geometry=spherical RTree2D.*' 2>&1 | tee spherical_overlap_1.txt
+sbt -java-home /usr/lib/jvm/jdk1.8.0 'charts -jvm /usr/lib/jvm/jdk-11/bin/java -p overlap=10 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=4,8,16 -p partToUpdate=0.1 -p geometry=spherical RTree2D.*' 2>&1 | tee spherical_overlap_10.txt
