db93a4e8d22d5f2a9ae937f55b2053e67bca9d10
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-08-02 16:55:06 +0200

    Switch benchmark running script to use OpenJDK 12

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sbt -java-home /usr/lib/jvm/jdk-11 -Dsbt.supershell=false clean 2>&1
-sbt -java-home /usr/lib/jvm/jdk-11 -Dsbt.supershell=false 'charts -p overlap=1 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=16 -p partToUpdate=0.1 -p geometry=plane .*' 2>&1 | tee plane_overlap_1.txt
-sbt -java-home /usr/lib/jvm/jdk-11 -Dsbt.supershell=false 'charts -p overlap=10 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=16 -p partToUpdate=0.1 -p geometry=plane .*' 2>&1 | tee plane_overlap_10.txt
-sbt -java-home /usr/lib/jvm/jdk-11 -Dsbt.supershell=false 'charts -p overlap=1 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=4,8,16 -p partToUpdate=0.1 -p geometry=spherical RTree2D.*' 2>&1 | tee spherical_overlap_1.txt
-sbt -java-home /usr/lib/jvm/jdk-11 -Dsbt.supershell=false 'charts -p overlap=10 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=4,8,16 -p partToUpdate=0.1 -p geometry=spherical RTree2D.*' 2>&1 | tee spherical_overlap_10.txt
+sbt -java-home /usr/lib/jvm/openjdk-12 -Dsbt.supershell=false clean 2>&1
+sbt -java-home /usr/lib/jvm/openjdk-12 -Dsbt.supershell=false 'charts -p overlap=1 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=16 -p partToUpdate=0.1 -p geometry=plane .*' 2>&1 | tee plane_overlap_1.txt
+sbt -java-home /usr/lib/jvm/openjdk-12 -Dsbt.supershell=false 'charts -p overlap=10 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=16 -p partToUpdate=0.1 -p geometry=plane .*' 2>&1 | tee plane_overlap_10.txt
+sbt -java-home /usr/lib/jvm/openjdk-12 -Dsbt.supershell=false 'charts -p overlap=1 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=4,8,16 -p partToUpdate=0.1 -p geometry=spherical RTree2D.*' 2>&1 | tee spherical_overlap_1.txt
+sbt -java-home /usr/lib/jvm/openjdk-12 -Dsbt.supershell=false 'charts -p overlap=10 -p rectSize=10 -p nearestMax=10 -p nodeCapacity=4,8,16 -p partToUpdate=0.1 -p geometry=spherical RTree2D.*' 2>&1 | tee spherical_overlap_10.txt
